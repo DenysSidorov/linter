4e362902cd527f034834412c5dc1a02f0fee72b1
Denys Sidorov <000scorpions000@gmail.com>
2017-10-19 16:19:04 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// https://www.npmjs.com/package/eslint-config-airbnb
+// https://www.npmjs.com/package/eslint-plugin-prettier
+// https://blog.jetbrains.com/webstorm/2016/08/using-external-tools/
+var a = 5;
+var b = {
+  a,
+  c: 'ff',
+};
---
+++ main2.js
@@ -0,0 +1,2 @@
+var t = 43;
+console.log(t);
